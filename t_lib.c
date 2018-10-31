97dded029613c3162bc56b9b61de84115f3b1023
Alina Christenbury <alina.christenbury@gmail.com>
2018-10-31 00:10:41 -0400

    an attempt at changing a data structure, it's kind of broken

--- t_lib.c
+++ t_lib.c
@@ -1,40 +1,41 @@
 #include "t_lib.h"
 
-ucontext_t *running;
-ucontext_t *ready;
+tcb *running;
+tcb *ready;
 
-/**
- * The calling thread voluntarily relinquishes the CPU,
- * and is placed at the end of the ready queue.
- * The first thread (if there is one) in the ready queue resumes execution.
- */
-void t_yield() {
-    ucontext_t *tmp;
-
-    tmp = running;
-    running = ready;
-    ready = tmp;
-
-    swapcontext(ready, running);
-}
 
 /**
  *  Initialize the thread library by setting up the "running" and the "ready" queues,
  *  creating TCB of the "main" thread, and inserting it into the running queue.
  */
 void t_init() {
-    ucontext_t *tmp;
-    tmp = (ucontext_t *) malloc(sizeof(ucontext_t));
 
-    getcontext(tmp);    /* let tmp be the context of main() */
-    running = tmp;
+    tcb *tmp_block;
+    tmp_block = malloc(sizeof(tcb));
+    tmp_block->thread_id = 0;
+    tmp_block->thread_priority = 0;
+    tmp_block->thread_context = (ucontext_t *) malloc(sizeof(ucontext_t));
+    tmp_block->next = NULL;
+
+    getcontext(tmp_block->thread_context);    /* let tmp be the context of main() */
+    running = tmp_block;
+
+    ready = tmp_block;
 }
 
 /**
  *  Shut down the thread library by freeing all the dynamically allocated memory.
  */
 void t_shutdown() {
-    //todo
+    free(running);
+    while (NULL != ready->next) {
+        tcb *temp_block = ready;
+        ready = ready->next;
+        free(temp_block->thread_context);
+        free(temp_block);
+    }
+    free(ready->thread_context);
+    free(ready);
 }
 
 /**
@@ -45,29 +46,44 @@ void t_shutdown() {
  *
  * TCB of the newly created thread is added to the *end* of the "ready" queue;
  * the parent thread calling `t_create()` continues its execution upon returning from `t_create()`.
- * @param fct
- * @param id
- * @param pri
+ * @param fct function
+ * @param id thread id
+ * @param pri priority
  * @return
  */
 int t_create(void (*fct)(int), int id, int pri) {
     size_t sz = 0x10000;
 
-    ucontext_t *uc;
-    uc = (ucontext_t *) malloc(sizeof(ucontext_t));
+    ucontext_t *new_context;
+    new_context = (ucontext_t *) malloc(sizeof(ucontext_t));
 
-    getcontext(uc);
+    getcontext(new_context);
 
-    uc->uc_stack.ss_sp = mmap(0, sz,
-                              PROT_READ | PROT_WRITE | PROT_EXEC,
-                              MAP_PRIVATE | MAP_ANON, -1, 0);
+    new_context->uc_stack.ss_sp = mmap(0, sz,
+                                       PROT_READ | PROT_WRITE | PROT_EXEC,
+                                       MAP_PRIVATE | MAP_ANON, -1, 0);
 
-    uc->uc_stack.ss_sp = malloc(sz);  /* new statement */
-    uc->uc_stack.ss_size = sz;
-    uc->uc_stack.ss_flags = 0;
-    uc->uc_link = running;
-    makecontext(uc, (void (*)(void)) fct, 1, id);
-    ready = uc;
+    new_context->uc_stack.ss_sp = malloc(sz);  /* new statement */
+    new_context->uc_stack.ss_size = sz;
+    new_context->uc_stack.ss_flags = 0;
+    new_context->uc_link = running->thread_context;
+    makecontext(new_context, (void (*)(void)) fct, 1, id);
+
+    //ready = new_context;
+
+    tcb *new_control_block;
+    new_control_block = malloc(sizeof(tcb));
+    new_control_block->thread_id = id;
+    new_control_block->thread_priority = pri;
+    new_control_block->thread_context = new_context;
+    new_control_block->next = NULL;
+
+    tcb *parent_control_block;
+    parent_control_block = ready;
+    while (NULL != parent_control_block->next) {
+        parent_control_block = parent_control_block->next;
+    }
+    parent_control_block->next = new_control_block;
 }
 
 /**
@@ -76,4 +92,28 @@ int t_create(void (*fct)(int), int id, int pri) {
  */
 void t_terminate() {
     //todo
+}
+
+/**
+ * The calling thread voluntarily relinquishes the CPU,
+ * and is placed at the end of the ready queue.
+ * The first thread (if there is one) in the ready queue resumes execution.
+ */
+void t_yield() {
+    tcb *current_thread;
+
+    current_thread = running;
+    running = ready;
+
+
+    tcb *tmp_control_block;
+    tmp_control_block = ready;
+
+    while (NULL != tmp_control_block->next) {
+        tmp_control_block = tmp_control_block->next;
+    }
+    tmp_control_block->next = current_thread;
+    ready = ready->next;
+
+    swapcontext(ready->thread_context, running->thread_context);
 }
